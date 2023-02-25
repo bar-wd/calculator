e45a48d1abacd2e1c4bb4c980c4694237d43265c
Ryan Korinek <korinekonie@gmail.com>
2023-02-24 18:01:31 -0800

    Display repeated squared properly
    Can hit the squared button repeatedly and will display it correctly

--- main.js
+++ main.js
@@ -4,21 +4,89 @@ const keys = document.querySelectorAll('.key');
 const screen = document.querySelector('.screen');
 const screenTop = document.querySelector('.screen-top');
 const screenBottom = document.querySelector('.screen-bottom');
+let lastOperandText;
+let lastOperand;
+let lastClassSelection;
+let percentNumber;
+let total = false;
+let numbers = [];
 
 function displayKey(event) {
   const textSelection = event.target.textContent;
   const classSelection = event.target.classList[1];
+  const operand = event.target.classList[2];
 
-  if (textSelection === 'C') {
-    screenBottom.innerText = '';
-    screenTop.innerText = '';
+  if (screenBottom.innerText === '0' && operand === 'number') {
+    screenBottom.innerText = textSelection;
   } else if (textSelection === 'CE') {
-    screenBottom.innerText = '';
+    screenBottom.innerText = '0';
+  } else if (classSelection === 'percentage') {
+    numbers.push(Number(screenBottom.innerText));
+    numbers.push(classSelection);
+    evaluate(numbers);
+    screenTop.innerText = `${total} ${lastOperandText} ${percentNumber}`;
+    screenBottom.innerText = `${total}`;
   } else if (classSelection === 'delete') {
     screenBottom.innerText = screenBottom.innerText.slice(0, -1);
+    if (!screenBottom.innerText) screenBottom.innerText = '0';
+  } else if (textSelection === 'C') {
+    screenTop.innerText = '';
+    screenBottom.innerText = '0';
+    numbers = [];
+  } else if (classSelection === 'squared') {
+    if (lastClassSelection === 'squared') {
+      screenTop.innerText = 'sqr(' + screenTop.innerText + ')';
+    } else {
+      screenTop.innerText = `sqr(${screenBottom.innerText})`;
+    }
+    numbers.push(Number(screenBottom.innerText));
+    numbers.push(classSelection);
+    evaluate(numbers);
+    screenBottom.innerText = `${total}`;
+  } else if (operand === 'operand') {
+    numbers.push(Number(screenBottom.innerText));
+    numbers.push(classSelection);
+    evaluate(numbers);
+    screenTop.innerText = `${total} ${textSelection}`;
+    screenBottom.innerText = `${total}`;
+  } else if (textSelection === '=') {
+    numbers.push(Number(screenBottom.innerText));
+    evaluate(numbers);
+    screenTop.innerText += ` ${screenBottom.innerText} =`;
+    screenBottom.innerText = total;
+  } else if (lastOperand === 'operand') {
+    screenBottom.innerText = textSelection;
   } else {
     screenBottom.innerText += textSelection;
   }
+  lastOperand = event.target.classList[2];
+
+  if (operand === 'operand') {
+    lastOperandText = textSelection;
+  }
+  lastClassSelection = classSelection;
+}
+
+function evaluate(input) {
+  const result = input.reduce((acc, curr, indx) => {
+    if (input[indx + 1] === 'percentage') {
+      curr = acc * (curr / 100);
+      percentNumber = curr;
+    }
+    if (input[indx] === 'squared') {
+      acc = acc * acc;
+    } else if (input[indx - 1] === 'addition') {
+      acc += curr;
+    } else if (input[indx - 1] === 'subtraction') {
+      acc -= curr;
+    } else if (input[indx - 1] === 'multiplication') {
+      acc *= curr;
+    } else if (input[indx - 1] === 'division') {
+      acc /= curr;
+    }
+    return acc;
+  });
+  total = result;
 }
 
 keys.forEach(key => key.addEventListener('click', displayKey));
